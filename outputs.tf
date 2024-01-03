d6269584d194eb46e1ef30cc3971d08dd81a0343
jad617 <elasmar.jad@gmail.com>
2024-01-03 17:19:27 -0500

    Migrating existing module as an standalone library

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "created_lambdas" {
+  description = "Output created Lambdas"
+  value       = aws_lambda_function.lambda
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "apps" {
+  description = "Map of all apps with their config"
+  type        = any
+}
+
+variable "common_tags" {
+  description = "Tags per brands"
+  type        = map(string)
+}
+
+variable "dynamodb_kms_key_arn" {
+  description = "DynamoDB KMS key arn"
+  type        = string
+  default     = ""
+}
+
+variable "lambdas" {
+  description = "Map of all lambda projects with config"
+  type        = any
+  default     = {}
+}
+
+variable "project_identifier" {
+  description = "The project name with environment"
+  type        = string
+}
